2abb7acbbe8cb12344631e9c6ff9f58afd5cec1b
Vu Nguyen <vu.nguyen@creativeforce.io>
2026-03-06 23:52:37 +0700

    update

--- client/src/components/PlayerHand.jsx
+++ client/src/components/PlayerHand.jsx
@@ -13,7 +13,7 @@ export default function PlayerHand({ myCards, lastMove, roomStatus, selectedCard
         setSelectedCards(cards)
         setTimeout(() => {
             setIsToggle(false)
-        }, 250)
+        }, 550)
     }
 
     // --- THUẬT TOÁN CHỌN BÀI THÔNG MINH ---
